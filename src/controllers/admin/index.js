dc4d887e8c1346994fc8d9fedeb42524c318d30a
Rafael de Souza almeida <109623681+RafaelAlmeid00@users.noreply.github.com>
2023-10-21 10:35:40 -0300

    Update index.js

--- src/controllers/admin/index.js
+++ src/controllers/admin/index.js
@@ -15,20 +15,61 @@ module.exports = {
         console.log('this is email: ', Email);
         console.log('this is password: ', password);
         if (Email != undefined) {
-            await bcrypt.compare(password, Email.adm_senha, (err, call) =>{
+            bcrypt.compare(password, Email.adm_senha, (err, call) => {
                 if (err || call == false) {
                     console.log('this is err: ', err);
                     console.log('this is call: ', call);
-                    res.status(401).send('erro burro burro macaco')
-                }else{
-                console.log('this is call: ', call);
-                var token = jwt.sign({
-                    Name: Email.adm_nome,
-                    Email: Email.adm_email,
-                    adm_id: Email.adm_id,
-                    adm_level: Email.adm_level
-                }, process.env.JWT_SECRET, { expiresIn: '7d' });
-                res.status(201).json({token: token});}
+                    res.status(401).send('erro burro burro macaco');
+                } else {
+                    console.log('this is call: ', call);
+                    var token = jwt.sign({
+                        adm_nome: Email.adm_nome,
+                        adm_email: Email.adm_email,
+                        adm_id: Email.adm_id,
+                        adm_level: Email.adm_level
+                    }, process.env.JWT_SECRET, { expiresIn: '7d' });
+                    res.status(201).json({ token: token });
+                }
+            })
+        }else{
+            res.status(401).send('senha ou email errado')
+        }
+
+    },
+
+    async Empresalogin(req, res) {
+        dotenv.config()
+        console.log('this is dotenv: ', dotenv);
+        const {cnpj: cnpj} = req.body;
+        const {password: password} = req.body;
+
+        const [CNPJ] = await knex('bussines').where('buss_CNPJ', '=', cnpj);
+        console.log('this is email: ', CNPJ);
+        console.log('this is password: ', password);
+        if (CNPJ != undefined) {
+            bcrypt.compare(password, CNPJ.buss_senha, (err, call) => {
+                if (err || call == false) {
+                    console.log('this is err: ', err);
+                    console.log('this is call: ', call);
+                    res.status(401).send('erro burro burro macaco');
+                } else {
+                    console.log('this is call: ', call);
+                    var token = jwt.sign({
+                        buss_CNPJ: CNPJ.buss_CNPJ,
+                        buss_nome: CNPJ.buss_nome,
+                        buss_contato: CNPJ.buss_contato,
+                        buss_FotoPerfil: CNPJ.buss_FotoPerfil,
+                        buss_endCEP: CNPJ.buss_endCEP,
+                        buss_endUF: CNPJ.buss_endUF,
+                        buss_endrua: CNPJ.buss_endrua,
+                        buss_endnum: CNPJ.buss_endnum,
+                        buss_endcomplemento: CNPJ.buss_endcomplemento,
+                        buss_endcidade: CNPJ.buss_endcidade,
+                        buss_tipo: CNPJ.buss_tipo,
+                        buss_status: CNPJ.buss_status
+                    }, process.env.JWT_SECRET, { expiresIn: '7d' });
+                    res.status(201).json({ token: token });
+                }
             })
         }else{
             res.status(401).send('senha ou email errado')
